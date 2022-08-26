98aba341697cb545b866b83675428d3b96993cec
portelote2004 <joaovictorportelote210@gmail.com>
2022-08-25 21:30:59 -0300

    Tentativa falha de colocar strings

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,6 +1,9 @@
 
-let name = String
-let email = String
-let proffisao = String
-let idade = Number
-let lugar = String
+let nome = "João Victor Portelote Machado"
+let email = "joaovictorportelote210@gmail.com"
+let profisao = "ProPlayer"
+let idade = "17"
+let lugar = "Londres"
+
+console.log(nome, email, profisao, idade, lugar)
+//Tentei colocar a frase dentro do console.log, não consegui

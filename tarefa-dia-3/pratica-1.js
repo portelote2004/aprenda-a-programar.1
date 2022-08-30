bba50f1484847708b4b6578e6955e63556b81a49
portelote2004 <joaovictorportelote210@gmail.com>
2022-08-30 19:00:06 -0300

    Operações de repetição

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,7 @@
+var n = 9
+
+while (n > 1) {
+    console.log(n)
+    n--;
+}
+console.log(n)
---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,8 @@
+var x = 9
+var n = 1
+
+while (x >= 9 && n <= 10) {
+    console.log(x * n)
+    n++
+}
+
